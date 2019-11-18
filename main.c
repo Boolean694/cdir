694259293455205ab3b9334e6cea9c51453f37de
Julian Wong <jwong01@stuy.edu>
2019-11-18 00:17:06 -0500

    hjalp

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 int main() {
@@ -10,13 +11,12 @@ int main() {
   int sz = 0;
   char *fn;
   printf("Directory being read: %s\n", dst->d_name);
-  struct stat ind = malloc(sizeof(ind));
-  while(dst != NULL) {
+  struct stat ind;
+  while((dst = readdir(".")) != NULL) {
     fn = dst->d_name;
     printf("File: %s\n", fn);
     stat(fn, &ind);
-    sz += ind->st_size;
-    dst = readdir(".");
+    sz += ind.st_size;
   }
   printf("Size of dir: %d\n", sz);
   return 0;
