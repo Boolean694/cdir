108143746f2f775af620b2fac926e50ea59c50dd
Julian Wong <jwong01@stuy.edu>
2019-11-18 00:08:01 -0500

    progress

--- main.c
+++ main.c
@@ -7,7 +7,17 @@ int main() {
   DIR *ret1 =  opendir(".");
   struct dirent *dst;
   dst = readdir(ret1);
-  printf("File being read: %s\n", dst->d_name);
-  printf("File type: %c\n", dst->d_type);
+  int sz = 0;
+  char *fn;
+  printf("Directory being read: %s\n", dst->d_name);
+  struct stat ind = malloc(sizeof(ind));
+  while(dst != NULL) {
+    fn = dst->d_name;
+    printf("File: %s\n", fn);
+    stat(fn, &ind);
+    sz += ind->st_size;
+    dst = readdir(".");
+  }
+  printf("Size of dir: %d\n", sz);
   return 0;
 }
